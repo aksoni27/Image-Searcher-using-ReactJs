02a78292e60f25277cc294bc9ffae387a3371491
Ankit Soni <ankitksoni1997@gmail.com>
2020-07-10 15:08:30 +0530

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+//const tailwindcss = require('tailwindcss');
+
+module.exports ={
+    plugins:[
+        //..
+        require('tailwindcss'),
+        require('autoprefixer'),
+        //..
+    ]
+   
+}
